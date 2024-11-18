1ef6e9c70805b3c72877ef544abf1bf6caee5b20
celinayk <celina.16161616@gmail.com>
2024-11-18 19:55:01 +0900

    싹다올림

---
+++ src/test/java/com/munhwahansang/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munhwahansang.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
